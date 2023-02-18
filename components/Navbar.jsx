4fc6c2b60049953acee4ae355b07025ede43c046
Wicho <54341712+Wichosu@users.noreply.github.com>
2023-02-17 21:08:39 -0600

    working on menu interaction

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,32 +3,42 @@ import styles from '@/styles/navbar.module.scss'
 import Toolbar from './Toolbar'
 import Menu from './Menu'
 import { useRouter } from 'next/router'
-import { useMenu } from '@/zustand/stores'
+import { useMenu, useSortBy } from '@/zustand/stores'
 
 export default function Navbar(){
   const router = useRouter();
   const showMenu = useMenu((state) => state.showMenu)
+  const menu = useMenu((state) => state.menu)
+  const sortBy = useSortBy((state) => state.sortBy)
+
+  const blur = menu
+  ? { filter: 'blur(4px)'}
+  : { filter: 'blur(0)'}
 
   return (
     <>
-      <div className={styles.navbar}>
-        <div className={styles.main}>
-          <img
-            className={styles.menu}
-            src='/menu.svg'
-            alt=''
-            onClick={showMenu}
-          />
-          <Link className={styles.link} href='/'><b>A</b>.DESIGN</Link>
-          <div>
-            <img src='/person.svg' alt='' />
-            <img src='/save.svg' alt='' />
-            <img src='/portfolio.svg' alt='' />
+      {!sortBy &&
+        <>
+          <div className={styles.navbar} style={blur}>
+            <div className={styles.main}>
+              <img
+                className={styles.menu}
+                src='/menu.svg'
+                alt=''
+                onClick={showMenu}
+              />
+              <Link className={styles.link} href='/'><b>A</b>.DESIGN</Link>
+              <div>
+                <img src='/person.svg' alt='' />
+                <img src='/save.svg' alt='' />
+                <img src='/portfolio.svg' alt='' />
+              </div>
+            </div>
+            {router.asPath === '/products' && <Toolbar />}
           </div>
-        </div>
-        {router.asPath === '/products' && <Toolbar />}
-      </div>
-      <Menu />
+          <Menu />
+        </>
+      }
     </>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -7,14 +7,23 @@ const mulish = Mulish({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
   const hideMenu = useMenu((state) => state.hideMenu)
+  const menu = useMenu((state) => state.menu)
+
+  const blur = menu
+  ? { filter: 'blur(4px)'}
+  : { filter: 'blur(0)'}
 
   return (
     <Layout>
       <main 
         className={mulish.className}
         onClick={hideMenu}
+        style={blur}
       >
-        <Component {...pageProps} />
+        {menu
+        ? <div style={{pointerEvents: 'none'}}><Component {...pageProps} /></div>
+        : <Component {...pageProps} />
+        }
       </main>
     </Layout>
   )
