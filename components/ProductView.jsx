09485a11bf20822ad6b158e796a02089e6b074e3
Wicho <54341712+Wichosu@users.noreply.github.com>
2023-03-04 21:31:11 -0600

    working on new styles for product view

--- components/ProductView.jsx
+++ components/ProductView.jsx
@@ -4,23 +4,37 @@ import { useState } from 'react'
 
 export default function ProductView({ img, name, desc, price}){
   return (
-    <>
-      <div>
-        <img src='' alt='' />
-        <img src='' alt='' />
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <img 
+          src={img} 
+          alt='' 
+          className={styles.img}
+        />
+        <img 
+          src='/portfolioBg.svg' 
+          alt='' 
+          className={styles.portfolio}  
+        />
       </div>
-      <div>
-        <div />
-        <div>
+      <div className={styles.info}>
+        <div className={styles.open} />
+        <div className={styles.price}>
           <p>{name}</p>
           <p>$ {price}</p>
         </div>
-        <p>{desc}</p>
-        <div>
-          <img src='' alt='' />
+        <p className={styles.desc}>{desc}</p>
+        <div className={styles.buttons}>
+          <img src='/save.svg' alt='' />
           <button>Select Size</button>
         </div>
+        <div className={styles.accordion}>Details & Care</div>
+        <p className={styles.panel}>Info...</p>
+        <div className={styles.accordion}>Size & Fit</div>
+        <p className={styles.panel}>Info...</p>
+        <div className={styles.accordion}>Features</div>
+        <p className={styles.panel}>Info...</p>
       </div>
-    </>
+    </div>
   )
 }
