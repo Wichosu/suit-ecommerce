ed6cdd29c81192f2434ccb7cde28c5b9fd3385f7
Wicho <54341712+Wichosu@users.noreply.github.com>
2023-03-11 21:27:41 -0600

    added cart page

---
+++ pages/cart.js
@@ -0,0 +1,23 @@
+
+export default function Cart(){
+  return(
+    <>
+      <h1>shopping bag $ </h1>
+      {/**Create a component for products on cart */}
+      <hr />
+      <div>
+        <p>delivery cost</p>
+        <p>free</p>
+      </div>
+      <div>
+        <p>total</p>
+        <p>$ </p>
+      </div>
+      <hr />
+      {/**Banner or logos */}
+      <img src='' alt='' />
+      <button>checkout</button>
+      <button>paypal</button>
+    </>
+  )
+}
