3768f48e0833f73a573418eb1d1a4d5bc1f68eb9
Wicho <54341712+Wichosu@users.noreply.github.com>
2023-03-12 22:12:23 -0600

    adding styles to cart

--- pages/cart.js
+++ pages/cart.js
@@ -1,23 +1,22 @@
+import styles from '@/styles/cart.module.scss'
 
 export default function Cart(){
   return(
-    <>
+    <div className={styles.cart}>
       <h1>shopping bag $ </h1>
       {/**Create a component for products on cart */}
       <hr />
-      <div>
-        <p>delivery cost</p>
-        <p>free</p>
-      </div>
-      <div>
-        <p>total</p>
-        <p>$ </p>
+      <div className={styles.bill}>
+        <p className={styles.delivery}>delivery cost <span>free</span></p>
+        <p className={styles.total}>total <span>$</span></p>
       </div>
       <hr />
       {/**Banner or logos */}
       <img src='' alt='' />
-      <button>checkout</button>
-      <button>paypal</button>
-    </>
+      <div className={styles.buttons}>
+        <button className={styles.checkout}>checkout</button>
+        <button className={styles.paypal}>paypal</button>
+      </div>
+    </div>
   )
 }
